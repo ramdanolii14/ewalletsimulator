446edc2b58069eaeb8abd32a5abee85a750f6132
Lucifersql <65667464+ramdanolii14@users.noreply.github.com>
2025-07-30 03:24:04 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 export const metadata = {
   title: "E-Wallet Simulator",
-  description: "Simulasi dompet digital by Ramdan",
+  description: "Simulasi e-wallet sederhana",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="id">
+      <body className="bg-gray-100 text-gray-800">
+        <Navbar />
+        <main className="p-4">{children}</main>
+      </body>
     </html>
   );
 }
